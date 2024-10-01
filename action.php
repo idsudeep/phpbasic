4a651ceceecfc034099adf9746468547588cb873
sudeep <sudeep.numb@gmail.com>
2024-10-01 08:19:35 +0545

    execute-mysqli_query-using-php-mysqli_query-method-and-also-redirected-using-php-header-method

--- action.php
+++ action.php
@@ -1,13 +1,27 @@
 <?php 
 
-echo "<pre>";
-print_r($_POST);
-
 if(isset($_POST['btn_user_form']) && $_GET['form']=='add_user'){
 
    $fname = $_POST['fname'];
    $email = $_POST['email'];
    $password = $_POST['password'];
    $contact = $_POST['contact'];
+   $address = $_POST['address'];
+
+   $query = "insert into users(fname ,email ,password ,contact,address)values('$fname','$email','$password',$contact ,'$address')";
+
+// mysqli_query() is a PHP method used run the sql query on php.
+
+if(mysqli_query($connect, $query)){
+
+    echo "user Created";
+
+    // To redirect header('location: locationName');
+
+    header("location:index.php?alert=success");
+
+}
+
+
 }
 ?>
